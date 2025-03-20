7a6f4503c7edb1d1d016d0580fe896a80ad1a9f5
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2025-03-20 18:17:40 +0000

    Refactor init_buffer | Rename variables | Add comments

--- ppmac_ushm_buffer.c
+++ ppmac_ushm_buffer.c
@@ -6,63 +6,61 @@
 #include "../../Include/pp_proj.h"
 #include "ppmac_ushm_buffer.h"
 
-//size_t get_block_size(char *types) {
+//size_t get_frame_size(char *types) {
 
 //    return strlen(types);
 //}
 
-inline size_t get_block_len(char *types) {
+inline size_t get_frame_len(char *types) {
     return strlen(types);
 }
 
 inline bool is_aligned(void* addr, size_t alignment) {
+    // return if remainder zero for addr divided by alignment
     return ((uintptr_t)addr % alignment) == 0;
 }
 
 inline void* align_addr(void *addr, size_t alignment){
     uintptr_t uint_addr = (uintptr_t) addr;
-    return (void *)((uint_addr + alignment-1) & ~(alignment -1));
+    return (void *)((uint_addr + alignment-1) & ~(alignment -1));   // uint_addr + alignment - 1
+}
+
+inline void set_aligned_pointer(void **ptr_arr, int idx, void **next_free_memory, size_t size, unsigned int shift) {
+    if (!is_aligned(*next_free_memory, size)) {
+        *next_free_memory = align_addr(*next_free_memory, size);
+    }
+    ptr_arr[idx] = (void *)((uintptr_t)pushm + ((uintptr_t)*next_free_memory >> shift));
+    *next_free_memory += size;
+    return;
 }
 
 int init_buffer(char *types, void *ptr_arr[]) {
     // TODO: add optional arg to get offset
-    // As it is, it is equivalent to what will be if receiving 1 as argument for offset
+
+    // As it is, it is equivalent to what will be if receiving 1 as argument for offset // TODO: Review this statement
     // That is necessary if using interleaved blocks - buffer size is also necessary in that case:
     // last_ocuppied_memory += buffer_size instead - it'd be better to find a better name for last_ocuppied_memory
     // Probably it needs a flag whether the blocks are interleaved or not - or use it when arg[0] exists
 
-    // Initialize buffer on USHM
-    size_t block_len = 0;
-    int idx; // loop iterator
-    void* last_ocuppied_memory = (void*)USHM_BASE_ADDR;
 
-    block_len = get_block_len(types);
-    if (block_len > MAX_BLOCK_SIZE) {
+    // Initialize buffer on USHM
+    size_t frame_len = 0;
+    int idx; // loop iterator
+    void* next_free_memory = (void*)USHM_BASE_ADDR;
+
+    frame_len = get_frame_len(types);
+    if (frame_len > MAX_FRAME_SIZE) {
         return -1;
     }
 
-    for (idx = 0; idx < block_len; idx++) {
+    for (idx = 0; idx < frame_len; idx++) {
         switch (types[idx]) {
             case 'i':
-                if (is_aligned(last_ocuppied_memory,sizeof(int))) {
-                    //Round up to the next alligned 4 bytes
-                    last_ocuppied_memory = align_addr(last_ocuppied_memory, sizeof(int));
-                }
-//                ptr_arr[idx] = (int *) pushm + (int)last_ocuppied_memory/sizeof(int);
-                ptr_arr[idx] = (int *)pushm + ((uintptr_t)last_ocuppied_memory >> 2);
-                last_ocuppied_memory += sizeof(int);
+                set_aligned_pointer(ptr_arr, idx, &next_free_memory, sizeof(double), 3);
                 break;
-
             case 'd':
-                if (is_aligned(last_ocuppied_memory,sizeof(double))) {
-                    //Round up to the next alligned 8 bytes
-                    last_ocuppied_memory = align_addr(last_ocuppied_memory, sizeof(double));
-                }
-//                ptr_arr[idx] = (double *) pushm + (int)last_ocuppied_memory/sizeof(double);
-                ptr_arr[idx] = (double *) pushm + ((uintptr_t)last_ocuppied_memory>>3);
-                last_ocuppied_memory += sizeof(double);
+                set_aligned_pointer(ptr_arr, idx, &next_free_memory, sizeof(double), 3);
                 break;
-
             default:
                 return -1;
         }
@@ -73,24 +71,24 @@ int init_buffer(char *types, void *ptr_arr[]) {
 int main(void)
 {
     // TODO: Make it into arg[] - Test with different settings
-    // Usage example: for a scan each block refers to a point in the trajectory
+    // Usage example: for a scan each frame refers to a point in the trajectory
     // They could be arranged subsequently or interleaved(not yet supported)
-    // Each block can contain MAX_BLOCK_SIZE values - Currently supports: int and double
-    char *block_types = "iiiddd";
+    // Each frame can contain MAX_FRAME_SIZE values - Currently supports: int and double
+    char *frame_types = "iiiddd";
 
-    // TODO: Make it into arg[] or calculate from block_types
-    // Necessary for offset between blocks - for initializing and setting values
-    size_t block_bytesize = 5*sizeof(double); // 5 bytes
+    // TODO: Make it into arg[] or calculate from frame_types
+    // Necessary for offset between frames - for initializing and setting values
+    size_t frame_bytesize = 5*sizeof(double); // 5 bytes
 
-    void *ptr_arr[MAX_BLOCK_SIZE];
+    void *ptr_arr[MAX_FRAME_SIZE];
 
     InitLibrary();  // Required for accessing Power PMAC library
-    init_buffer(block_types, ptr_arr); // TODO: return to status variable to check errors
+    init_buffer(frame_types, ptr_arr); // TODO: return to status variable to check errors
     int idx;
 
     // TEST: Print the values stored at the addresses
-    for (idx = 0; idx < get_block_len(block_types); idx++) {
-        switch (block_types[idx]) {
+    for (idx = 0; idx < get_frame_len(frame_types); idx++) {
+        switch (frame_types[idx]) {
             case 'i':
                 printf("Value at ptr_arr[%d]: %d\n", idx, *(int *)ptr_arr[idx]);
                 printf("Addr at ptr_arr[%d]: 0x%08x\n", idx, ptr_arr[idx]);
--- ppmac_ushm_buffer.h
+++ ppmac_ushm_buffer.h
@@ -5,12 +5,15 @@
 #define USHM_BASE_ADDR    4000
 #define USHM_BUFF_SIZE    1000
 //#define _PVT_MODE_
-#define _RT_MODE_                // _PVT_MODE_ or _RT_MODE_
+#define _RT_MODE_           // _PVT_MODE_ or _RT_MODE_
 ////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-#define MAX_BLOCK_SIZE 20
+#define MAX_FRAME_SIZE 20
+
+#define INT_SHIFT       2
+#define DOUBLE_SHIFT    3
 
 #ifdef _RT_MODE_
     #define DOUBLE_PER_POINT        NUM_AXES
@@ -18,6 +21,8 @@
     #define DOUBLE_PER_POINT        2*NUM_AXES
 #endif
 
+// TODO: Refactor above to use bitshift instead of multiplication/division
+// Is this valid for both modes: _RT_MODE_ and _PVT_MODE_
 // Offset between points
 #define USHM_POINT_OFFSET_ADDR            (int)(2*sizeof(int)+DOUBLE_PER_POINT*sizeof(double))
 #define USHM_POINT_OFFSET_INT_IDX         (int)(USHM_LINE_OFFSET_ADDR/sizeof(int))
@@ -27,4 +32,5 @@
 #define USHM_BUFF_OFFSET_ADDR            (int)((USHM_BUFF_SIZE)*USHM_LINE_OFFSET_ADDR)
 #define USHM_BUFF_OFFSET_INT_IDX         (int)(USHM_BUFF_OFFSET_ADDR/sizeof(int))
 #define USHM_BUFF_OFFSET_DOUBLE_IDX      (int)(USHM_BUFF_OFFSET_ADDR/sizeof(double))
+
 ////////////////////////////////////////////////////////////////////////////////////////
